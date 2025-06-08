553c74f8579fee0b5ac0764d99958db6af0d90fe
KhaNguyen454 <nkha08462@gmail.com>
2025-06-08 12:19:17 +0700

    Fisrt Project

---
+++ src/main/java/com/BE/QuitCare/QuitCareApplication.java
@@ -0,0 +1,13 @@
+package com.BE.QuitCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuitCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuitCareApplication.class, args);
+	}
+
+}
