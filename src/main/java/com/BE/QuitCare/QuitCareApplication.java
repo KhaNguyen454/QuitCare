4c8cd99de23bd2f1cdbb8ef28df2afcbce394381
KhaNguyen454 <nkha08462@gmail.com>
2025-06-08 21:55:09 +0700

    API login and register completed

--- src/main/java/com/BE/QuitCare/QuitCareApplication.java
+++ src/main/java/com/BE/QuitCare/QuitCareApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "Information"))
+@OpenAPIDefinition(info = @Info(title = "Quit Care API", version = "1.0", description = "Information"))
 @SecurityScheme(name = "api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class QuitCareApplication {
 	public static void main(String[] args) {
