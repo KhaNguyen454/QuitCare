5e294ba2cd3f3b01d3b178582ff3f52619efdd99
NguyenTrungNghia04 <nghiantse184512@fpt.edu.vn>
2025-07-03 07:58:21 +0700

    add entity UserAchievement

---
+++ src/main/java/com/BE/QuitCare/enums/AchievementType.java
@@ -0,0 +1,10 @@
+package com.BE.QuitCare.enums;
+
+public enum AchievementType
+{
+    NO_SMOKING_ONE_DAY,
+    NO_SMOKING_THREE_DAYS,
+    REDUCED_10_CIGS_IN_WEEK,
+    SAVED_100K_VND,
+    LOGGED_7_DAYS_CONSECUTIVELY
+}
